09f4b4b330bc232ba502313d225496e2a06584d7
JavaCommons Technologies <javacommons@gmail.com>
2024-07-04 15:11:30 +0900

    .

---
+++ oop.el
@@ -0,0 +1,45 @@
+(require 'eieio)
+
+(defmacro !class ($class $super &rest $spec-list)
+  (if (not (listp $spec-list))
+      (error "$spec list is not list")
+    (let ( $spec-list2 )
+      (dolist ($spec $spec-list)
+        (setq $spec-list2 (nconc $spec-list2 (list (!class::spec $spec))))
+        )
+      (let (($form `(defclass ,$class ,$super ,$spec-list2)))
+        ;;(xdump $form)
+        $form
+        )
+      )
+    )
+  )
+
+(defun !class::spec ($spec)
+  (let* (($sym-name (!class::symbol-name $spec))
+         ($ini-form (!class::init-form $spec))
+         )
+    `( ,(intern $sym-name)
+       :initarg ,(intern (concat ":" $sym-name))
+       :initform ,$ini-form )
+    )
+  )
+
+(defun !class::symbol-name ($spec)
+  (let* (($sym $spec))
+    (if (listp $sym)
+        (setq $sym (nth 0 $sym)))
+    (let* (($sym-name (symbol-name $sym)))
+      (if (string-match "^:" $sym-name)
+          (setf $sym-name (substring $sym-name 1)))
+      $sym-name
+      )
+    )
+  )
+
+(defun !class::init-form ($spec)
+  (if (symbolp $spec)
+      nil
+    (nth 1 $spec)))
+
+(provide 'oop)
